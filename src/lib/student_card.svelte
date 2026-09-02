8e86c957a288f64ba4b0a8d8a3b9aa91fd730405
Chewsy <13772434+Chewsy@users.noreply.github.com>
2026-09-02 23:19:55 +0200

    #9 student-card component gebouwt

---
+++ src/lib/student_card.svelte
@@ -0,0 +1,28 @@
+<script>
+    let { person } = $props();
+</script>
+
+<article class="student-card">
+    {#if person.image}
+        <img src={person.image} alt="Foto van {person.name}" />
+    {/if}
+    <p>{person.name}</p>
+</article>
+
+<style>
+    .student-card {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+        padding: 1rem;
+        background-color: aquamarine;
+        border-radius: 10px;
+
+        & img {
+            width: 100%;
+            height: 11rem;
+            object-fit: cover;
+            border-radius: 10px;
+        }
+    }
+</style>
