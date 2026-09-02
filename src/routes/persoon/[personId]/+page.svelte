ab9e2947624e06af81c17827d42c0e7b217f8c36
handouh1 <heba.handouka@hva.nl>
2026-09-03 01:05:34 +0200

    #6 inladen getest + header

---
+++ src/routes/persoon/[personId]/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	let { data } = $props();
+	let person = data.person;
+</script>
+
+<a href="/">← Back to overview</a>
+
+<header>
+	{#if person.avatar}
+		<img src={person.avatar} alt={person.name} />
+	{/if}
+
+	<div>
+		<h1>{person.name}</h1>
+		{#if person.github_handle}
+			<p>@{person.github_handle}</p>
+		{/if}
+
+		{#if person.birthdate}
+			<p>{person.birthdate}</p>
+		{/if}
+		{#if person.residency}
+			<p>{person.residency}</p>
+		{/if}
+	</div>
+</header>
