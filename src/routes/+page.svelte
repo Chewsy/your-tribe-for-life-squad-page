008002d11664e4f2c6ae45255b79668ed0c9d083
a7lam <ahlam.adda@hva.nl>
2026-09-03 00:24:26 +0200

    #12 first commit + merge solved

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,3 +5,13 @@
 </script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+{#each persons as squad}
+
+
+{/each}
+
+<style>
+
+</style>
