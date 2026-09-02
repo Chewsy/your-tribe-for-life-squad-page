99c83853ddc9deea7042fe964ff55dc52a6702d5
Chewsy <13772434+Chewsy@users.noreply.github.com>
2026-09-02 21:31:07 +0200

    #9 grid HTML prototype
    Aanpassingen:
    
    - Main gemaakt
    - Grid display toegepast op de main
    - Tijdelijke styling toegepast op de kaarten voor visualisatie code resultaten

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,29 @@
 <script>
-    let {data} = $props();
+    let { data } = $props();
 
     const persons = $derived(data.persons);
 </script>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<main class="grid">
+    <article class="student-card"></article>
+    <article class="student-card"></article>
+    <article class="student-card"></article>
+    <article class="student-card"></article>
+</main>
+
+<style>
+    .grid {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-template-rows: repeat(auto, 1fr);
+        gap: 10px;
+        flex-wrap: wrap;
+    }
+
+    article {
+        width: 300px;
+        height: 300px;
+        background-color: aquamarine;
+        border-radius: 10px;
+    }
+</style>
