9769d3c730ed5f0ef72a1c1b2bf02d7e0601f9ca
a7lam <ahlam.adda@hva.nl>
2026-09-03 11:22:15 +0200

    #12 new filtered fetch + each block for cards + card information dynamic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,20 @@
 <script>
+    import Card from '$lib/Card.svelte';
     let {data} = $props();
 
     const persons = $derived(data.persons);
 </script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-
-{#each persons as squad}
-
-
-{/each}
+<ul>
+    {#each persons as person}
+        <li>
+            <Card {person} />
+        </li>
+    {/each}
+</ul>
 
 <style>
 
