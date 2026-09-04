935dcec121f7898ed4db7291c7b9ce7bbcbe4975
Chewsy <13772434+Chewsy@users.noreply.github.com>
2026-09-04 21:50:02 +0200

    #16 ✨feat: HTML en logica error handling voor persoon route af

--- src/routes/persoon/[personId]/+page.server.js
+++ src/routes/persoon/[personId]/+page.server.js
@@ -1,6 +1,17 @@
-export async function load({ params, fetch }) {
-	const response = await fetch(`https://fdnd.directus.app/items/person/${params.personId}`);
-	const { data: person } = await response.json();
+// Documentatie: https://svelte.dev/docs/kit/errors
+// Svelte heeft een helper functie die je kan importeren en alle error code handling voor je doet
+// De resultaten van de handler worden op src/routes/+error.svelte weergeven
 
+import { error } from '@sveltejs/kit';
+
+export async function load({ params }) {
+	const res = await fetch(`https://fdnd.directus.app/items/person/${params.personId}`);
+
+	// Als de response niet (!) ok is, weergeef de error 404 met de hardcoded error bericht
+	if (!res.ok) {
+		error(404, { message: 'Oh nee, deze pagina bestaat niet!' });
+	}
+
+	const { data: person } = await res.json();
 	return { person };
 }
