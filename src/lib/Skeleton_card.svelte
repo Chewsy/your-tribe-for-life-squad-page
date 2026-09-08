671fed69e0457dd06ae79b5bf94a01ca6dfc79ca
Chewsy <13772434+Chewsy@users.noreply.github.com>
2026-09-08 22:17:19 +0200

    #14 💄style: skeleton state styling nu als Studentcard
    en --skeleton-gray custom css token toegevoegd in styleguide

--- src/lib/Skeleton_card.svelte
+++ src/lib/Skeleton_card.svelte
@@ -11,25 +11,33 @@
 		display: flex;
 		flex-direction: column;
 		border: 0.094rem solid hsl(0, 0%, 77%);
-		border-radius: var(--radius);
+		border-radius: var(--radius-card);
 		overflow: hidden;
 	}
 
 	.skeleton-img {
 		height: 9.375rem;
 		background: hsl(0, 0%, 93%);
+		border-top-left-radius: var(--radius-card);
+		border-top-right-radius: var(--radius-card);
 	}
 
 	.skeleton-info {
 		display: flex;
+		align-items: center;
 		flex-direction: row;
+		justify-content: space-between;
+		background: transparent;
 		gap: var(--gap);
 		padding: var(--space-sm);
+		border-radius: var(--radius-card);
+		border-top-left-radius: 0;
+		border-top-right-radius: 0;
 	}
 
 	.skeleton-text {
 		height: var(--space-md);
-		width: 100%;
+		width: 80%;
 		max-width: 12.5rem;
 		border-radius: var(--radius);
 		background: hsl(0, 0%, 93%);
