3f826a6a8e3de32746ae9df921629115068ccc5c
Chewsy <13772434+Chewsy@users.noreply.github.com>
2026-09-11 21:16:43 +0200

    #14 🐛fix: skeleton state aangepast naar nieuwe card stijl

--- src/lib/Skeleton_card.svelte
+++ src/lib/Skeleton_card.svelte
@@ -1,7 +1,10 @@
 <article class="skeleton">
 	<div class="skeleton-img"></div>
 	<div class="skeleton-info">
-		<div class="skeleton-text"></div>
+		<div class="skeleton-text-age">
+			<div class="skeleton-text"></div>
+			<div class="skeleton-age"></div>
+		</div>
 		<div class="skeleton-icon"></div>
 	</div>
 </article>
@@ -9,53 +12,57 @@
 <style>
 	.skeleton {
 		position: relative;
-		display: flex;
-		flex-direction: column;
-		border: 0.094rem solid hsl(0, 0%, 77%);
-		border-radius: var(--radius-card);
+		height: 100%;
 		overflow: hidden;
-	}
-
-	.skeleton-img,
-	.skeleton-text,
-	.skeleton-icon {
-		background: hsl(0, 0%, 93%);
+		border: 1px solid var(--skeleton-gray);
+		border-radius: var(--radius-card);
+		background: var(--white);
+		box-shadow: 0 4px 12px var(--skeleton-gray);
 	}
 
 	.skeleton-img {
-		height: 9.375rem;
-		border-top-left-radius: var(--radius-card);
-		border-top-right-radius: var(--radius-card);
+		aspect-ratio: 4 / 3;
+		background: var(--skeleton-gray);
 	}
 
 	.skeleton-info {
 		display: flex;
 		align-items: center;
-		flex-direction: row;
-		justify-content: space-around;
-		padding-left: var(--space-sm);
-		padding-right: var(--space-sm);
-		gap: var(--gap);
-		border-radius: var(--radius-card);
-		border-top-left-radius: 0;
-		border-top-right-radius: 0;
-		height: var(--space-md);
+		justify-content: space-between;
+		gap: var(--space-md);
+		height: 4.8rem;
+		padding: var(--space-md);
+	}
+
+	.skeleton-text-age {
+		display: flex;
+		flex-direction: column;
+		gap: 0.25rem;
+		flex: 1 1 auto;
 	}
 
 	.skeleton-text {
-		height: var(--space-sm);
-		width: 80%;
+		width: 100%;
 		max-width: 12.5rem;
+		height: 1.05rem;
 		border-radius: var(--radius);
+		background: var(--skeleton-gray);
+	}
+
+	.skeleton-age {
+		width: 50%;
+		max-width: 12.5rem;
+		height: 0.875rem;
+		border-radius: var(--radius);
+		background: var(--skeleton-gray);
 	}
 
 	.skeleton-icon {
-		height: var(--space-md);
-		width: var(--space-md);
+		width: 2.25rem;
+		height: 2.25rem;
 		border-radius: 50%;
+		background: var(--skeleton-gray);
 	}
-
-	/* Animatie bovenop skeleton card */
 
 	.skeleton::after {
 		content: "";
@@ -64,7 +71,7 @@
 		background: linear-gradient(
 			110deg,
 			transparent 40%,
-			hsl(0, 0%, 100%, 0.35) 50%,
+			hsl(0, 0%, 100%, 35%) 50%,
 			transparent 60%
 		);
 		transform: translateX(-100%);
